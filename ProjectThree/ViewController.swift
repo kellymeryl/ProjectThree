1660622d758ed20119ea1a35afdcb99b837d6a43
Kelly McNevin <kellymcnevin@Bruces-MacBook-Pro.local>
2017-01-13 13:39:32 -0500

    Initial Commit

---
+++ ProjectThree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectThree
+//
+//  Created by Kelly McNevin on 1/13/17.
+//  Copyright © 2017 Kelly McNevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
