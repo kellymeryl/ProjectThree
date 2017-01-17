6f2b2b1bbebfe476fa7a7bf335862358fb4df9e4
Kelly McNevin <kellymcnevin@Bruces-MacBook-Pro.local>
2017-01-17 17:03:26 -0500

    add sign up

--- ProjectThree/ConsumerSignUpViewController.swift
+++ ProjectThree/ConsumerSignUpViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class ConsumerSignUpViewController: UIViewController {
 
     @IBOutlet weak var consumerUsernameTextField: UITextField!
@@ -21,10 +22,47 @@ class ConsumerSignUpViewController: UIViewController {
     }
     @IBAction func consumerSignUpButtonWasTapped(_ sender: Any) {
         //signs consumer up as user
+        
+        if consumerUsernameTextField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter a valid username", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else if consumerPasswordTextField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter a valid password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else if consumerPasswordTextField.text != consumerConfirmPasswordTextField.text {
+            let alertController = UIAlertController(title: "Error", message: "Passwords do not match", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else if consumerUsernameTextField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter a valid username", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else if consumerConfirmPasswordTextField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please confirm password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else {
+            newUserSignup(viewController: self, emailTextField: consumerEmailTextField.text!, passwordTextField: consumerPasswordTextField.text!)
+        }
+        
     }
     
     @IBAction func consumerAlreadyAUserButtonWasTapped(_ sender: Any) {
         //segues to consumer login page
+        
+        
     }
     @IBAction func consumerSignUpAsVendorWasTapped(_ sender: Any) {
         //segues to vendor login page
