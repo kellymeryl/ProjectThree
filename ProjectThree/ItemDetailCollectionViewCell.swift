c5e13bae22eb8a2a7ab7c3a6ac77d8839b2ff71e
Andrew Moskwoitz <andrewmoskowitz@gmail.com>
2017-01-16 22:57:57 -0500

    Added item detail screen and collection view for detail pics

---
+++ ProjectThree/ItemDetailCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemDetailCollectionViewCell.swift
+//  ProjectThree
+//
+//  Created by Andrew Moskowitz on 1/16/17.
+//  Copyright © 2017 Kelly McNevin. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+}
