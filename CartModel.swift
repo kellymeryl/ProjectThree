f5d14d71e241259914ea9ad732b1a50548901faf
Kelly McNevin <kellymcnevin@Bruces-MacBook-Pro.local>
2017-01-18 16:40:56 -0500

    add core data

---
+++ CartModel.swift
@@ -0,0 +1,30 @@
+//
+//  CartModel.swift
+//  ProjectThree
+//
+//  Created by Kelly McNevin on 1/18/17.
+//  Copyright © 2017 Kelly McNevin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CartModel {
+    
+  //  let context = (UIApplication.shared.delegate as! AppDelegate).persistentStoreCoordinator.c
+    
+    
+    func addToCart(item: Item) {
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+}
