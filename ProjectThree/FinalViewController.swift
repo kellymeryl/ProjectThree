a61f9c663942e05a3769a3fe3071c9f2a3860acb
Kelly McNevin <kellymcnevin@Bruces-MacBook-Pro.local>
2017-01-24 00:03:43 -0500

    updated UI

---
+++ ProjectThree/FinalViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FinalViewController.swift
+//  ProjectThree
+//
+//  Created by Kelly McNevin on 1/24/17.
+//  Copyright © 2017 Kelly McNevin. All rights reserved.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+    @IBAction func homeScreenButtonWasTapped(_ sender: Any) {
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
